c4c805fe67caa79d6f4e134e2417129cfb23e1e0
Taro Mieno <tmieno2@unl.edu>
2024-04-30 13:11:38 -0500

    added fairway grid polygon data

--- R/data.R
+++ R/data.R
@@ -2,27 +2,37 @@
 #'
 #' A subset of wells registered in Nebraska
 #'
-#' @format ## `wells_ne`
+#' @format data.frame
 #' A data frame with 1,000 rows and 3 columns:
 #' \describe{
 #'   \item{wellid}{Well ID number}
 #'   \item{longdd}{Longitude}
 #'   \item{latdd}{Latitude}
-#'   ...
 #' }
 #' @source <https://dnr.nebraska.gov/groundwater>
 "wells_ne"
 
-#' Mower sensor data in csv
+#' Mower sensor data
 #'
-#' @format ## `mower_sensor`
+#' @format `data.frame`
 #' A data frame with 420 rows and 3 columns:
 #' \describe{
 #'   \item{LNG}{Longitude}
 #'   \item{LAT}{Latitude}
 #'   \item{ELEV}{Elevation}
-#'   ...
 #' }
 #' @source
 "mower_sensor"
 
+#' Fairway plot data
+#'
+#' Fairway plot polygons saved as an sf object
+#'
+#' @format `sf`
+#' A data frame with 42 rows and 2 columns:
+#' \describe{
+#'   \item{grid}{grid ID}
+#'   \item{geometry}{geometry}
+#' }
+#' @source
+"fairway_grid"
