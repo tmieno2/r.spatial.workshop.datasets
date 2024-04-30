755b53a940ca463fe2e802f016cd669515340923
Taro Mieno <tmieno2@unl.edu>
2024-04-30 09:33:20 -0500

    create a new dataset wells_ne

---
+++ R/data.R
@@ -0,0 +1,14 @@
+#' Wells in Nebraska with geographic coordinates
+#'
+#' A subset of wells registered in Nebraska
+#'
+#' @format ## `wells_ne`
+#' A data frame with 1,000 rows and 3 columns:
+#' \describe{
+#'   \item{wellid}{Well ID number}
+#'   \item{longdd}{Longitude}
+#'   \item{latdd}{Latitude}
+#'   ...
+#' }
+#' @source <https://www.who.int/teams/global-tuberculosis-programme/data>
+"wells_ne"
