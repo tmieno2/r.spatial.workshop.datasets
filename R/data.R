809e7dc48f09f834aa2ecc0a38929df2cdd8ce20
Taro Mieno <tmieno2@unl.edu>
2024-05-04 16:46:48 -0500

    High Plains aquifer boundary and rails roads in Nebraska added as datasets

--- R/data.R
+++ R/data.R
@@ -21,7 +21,7 @@
 #'   \item{LAT}{Latitude}
 #'   \item{ELEV}{Elevation}
 #' }
-#' @source
+#' @source privately obtained
 "mower_sensor"
 
 #' Fairway plot data
@@ -29,10 +29,35 @@
 #' Fairway plot polygons saved as an sf object
 #'
 #' @format `sf`
-#' A data frame with 42 rows and 2 columns:
+#' An sf with 42 rows and 2 columns:
 #' \describe{
 #'   \item{grid}{grid ID}
 #'   \item{geometry}{geometry}
 #' }
-#' @source
+#' @source privately obtained
 "fairway_grid"
+
+#' High-Plains Aquifer boundary data
+#'
+#' High-Plains Aquifer boundary polygon data saved as an sf object
+#'
+#' @format `sf`
+#' An sf with 2 rows and 1 columns:
+#' \describe{
+#'   \item{geometry}{geometry}
+#' }
+#' @source <https://www.sciencebase.gov/catalog/item/6314061bd34e36012efa397b>
+"hp_boundary"
+
+#' US railroads in Nebraska
+#'
+#' US railroads stored as an sf object
+#'
+#' @format `sf`
+#' An sf with 2 rows and 1 columns:
+#' \describe{
+#'   \item{LINEARID}{rail ID}
+#'   \item{geometry}{geometry}
+#' }
+#' @source <"http://www2.census.gov/geo/tiger/TIGER2019/RAILS/tl_2019_us_rails.zip">
+"railroads_ne"
---
+++ README.md
@@ -0,0 +1,12 @@
+
+<!-- README.md is generated from README.Rmd. Please edit that file -->
+
+# r.spatial.workshop.datasets
+
+<!-- badges: start -->
+<!-- badges: end -->
+
+The `r.spatial.workshop.datasets` package is a data pacakge. Its
+datasets are mostly used internally in the lecture slides in the
+[R-as-GIS-workshop](https://github.com/tmieno2/R-as-GIS-workshop)
+repository.
